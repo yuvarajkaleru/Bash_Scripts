6f2eb6abfcdec52561402f5e1194969779d9cda9
yuvarajkaleru <yuvarajvanjari1@gmail.com>
2025-12-28 18:20:40 +0530

    AWS PROJECT

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##################################################
+#                Author: Yuvaraj                 #
+#                Date: 27-12-2025                #
+#                  Version: v1                   #
+#                                                #
+# This script will report the AWS resource usage #
+##################################################
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+# list S3 Buckets
+echo "S3 Buckets Details\n"
+aws s3 ls
+
+# list EC2 Instances
+echo "EC2 Insatances Details\n"
+aws ec2 describe-instances
+
+# list Lambda
+echo "Lambda Details\n"
+aws lambda list-functions 
+
+# list IAM Users
+echo "IAM Users Details\n"
+aws iam list-users
